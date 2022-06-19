d5806caab5f54c7258dae208afe115513bf704d8
william40614 <william40614@gmail.com>
2022-06-19 21:34:39 +0800

    2022/6/19.1

--- server.js
+++ server.js
@@ -6,18 +6,17 @@ const addr = './data.json';
 const app = express();
 
 //var urlencodedParser = bodyParser.urlencoded({ extended: false });
+let JSONdata = {};
 
 app.post('/key', bodyParser, async function(req,res){
     //console.log('encode ' + unescape(req.body.data));
     //console.log(req.body.data);
     //console.log(req.body.key);
-    JSONdata = "";
     await editdata.getdata(addr).then(dat => {
         JSONdata = JSON.parse(dat);
     });
-    var data_tmp = {};
     re_flag = false;
-    for( i=0; i < JSONdata.length; i++){
+    for( i=0; i < JSONdata.data_array.length; i++){
         if(JSONdata.data_array[i].key == req.body.key){
             re_flag = true;
             res.status(400);
@@ -26,14 +25,33 @@ app.post('/key', bodyParser, async function(req,res){
         }
     }
     if(re_flag == false){
-        data_tmp.key = req.body.key;
-        data_tmp.value = req.body.value;
+        var data_tmp = {
+            "key" : req.body.key,
+            "value" : req.body.value
+        }
+        //JSONdata[JSONdata_l] = JSONdata[JSONdata_l] || [];           // <========
         JSONdata.data_array.push(data_tmp);
+        //JSONdata = JSON.stringify(temp);
+        await editdata.writedata(addr,JSON.stringify(JSONdata));
         res.status(201);
         res.end();
-        await editdata.writedata(addr,JSON.stringify(JSONdata));
     }
 });
+
+/*app.get('/key', bodyParser, async function(req,res){
+    //console.log('encode ' + unescape(req.body.data));
+    //console.log(req.body.data);
+    //console.log(req.body.key);
+    await editdata.getdata(addr).then(dat => {
+        JSONdata = JSON.parse(dat);
+    });
+    var tmp = [];
+    for( i=0; i<JSONdata.data_array.length;i++){
+        tmp.push()
+    }
+    res.status(201);
+    res.end();
+});*/
 
 app.listen(port, () => {
     console.log(`server app listening at http://localhost:${port}`)
